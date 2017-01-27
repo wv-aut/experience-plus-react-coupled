3bbdded5c5ecadc7ae28d3735c4dd1d306ce669f
Andreas <andreas.schadauer@gmail.com>
2017-01-27 10:31:56 +0100

    Tweak required fields settings

--- src/routes/User/config/requiredFields.config.js
+++ src/routes/User/config/requiredFields.config.js
@@ -7,14 +7,26 @@ import { TAX_RECEIPT_PROFILE_ROUTE } from 'routes/config/routes.config'
 
 
 export function _locationToObjectKey (location) {
-  return location.replace(/-/, '_').replace(/\//, '')
+  return location.replace(/-/, '_').replace(/^\//, '')
 }
 
 export const FORM_ELEMENTS = {
   [_locationToObjectKey(TAX_RECEIPT_PROFILE_ROUTE)]: {
-    [API.FIRST_NAME]: true,
-    [API.LAST_NAME]: true,
-    [API.SALUTATION_CODE]: true,
+    [API.FIRST_NAME]: {
+      [API.REGISTERED_COMPANY]: {
+        value: false
+      }
+    },
+    [API.LAST_NAME]: {
+      [API.REGISTERED_COMPANY]: {
+        value: false
+      }
+    },
+    [API.SALUTATION_CODE]: {
+      [API.REGISTERED_COMPANY]: {
+        value: false
+      }
+    },
     [API.EMAIL]: true,
     [API.TAX_OPTOUT]: true,
     [API.BIRTH_DATE]: {
@@ -25,5 +37,39 @@ export const FORM_ELEMENTS = {
   }
 }
 
+/**
+ * Check if current field is required
+ * @param {string} fieldName - i.e. firstName
+ * @param {object} userData - full set of user data
+ * @param {string} pathname - i.e. /user-data
+ * @return {boolean}
+ */
+export function checkIfFieldIsRequired (fieldName, userData, pathname) {
+  const key = _locationToObjectKey(pathname)
+  const fieldValue = FORM_ELEMENTS[key][fieldName]
+  let isRequired = false
+  if (typeof fieldValue !== 'undefined') {
+    // If there are no dependencies
+    if (fieldValue === true) {
+      isRequired = true
+    } else {
+      // We check the dependencies
+      for (let element in fieldValue) {
+        if (fieldValue[element]['value'] === userData[element]) {
+          isRequired = true
+        }
+      }
+    }
+  }
+  return isRequired
+}
 
-
+export function showErrorMessage (value, minValue = false, maxValue = false) {
+  let addClass = 'ok'
+  if (minValue) { addClass = (Number(value) <= minValue) ? 'error-less' : 'ok' }
+  if (maxValue) { addClass = (Number(value) >= maxValue) ? 'error-exceed' : 'ok' }
+  if (typeof value === 'undefined' || value === '' || value === '0' || value === '00') {
+    addClass = 'error'
+  }
+  return addClass
+}
