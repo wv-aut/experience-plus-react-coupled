5a2108720d1ef48a82832f88be36d87bfc8b8bd8
Andreas <andreas.schadauer@gmail.com>
2017-01-31 16:36:44 +0100

    Change location of App container

--- src/components/App/containers/AppContainer.js
+++ src/components/App/containers/AppContainer.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react'
 import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
 
+import Auth from '../../Auth/containers/AuthContainer'
+
 class AppContainer extends Component {
   static propTypes = {
     routes : PropTypes.object.isRequired,
@@ -14,10 +16,10 @@ class AppContainer extends Component {
 
   render () {
     const { routes, store } = this.props
-
     return (
       <Provider store={store}>
         <div style={{ height: '100%' }}>
+          <Auth />
           <Router history={browserHistory} children={routes} />
         </div>
       </Provider>
