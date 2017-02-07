fd1c4447a11c675123e8bd944629eee7e51ce3cd
Andreas <andreas.schadauer@gmail.com>
2017-02-07 14:33:12 +0100

    Replace fetch URL

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,12 +3,14 @@ import { fetchUserProfile } from '../routes/User/modules/user'
 // ------------------------------------
 // Constants
 // ------------------------------------
-export const FETCH_TEMP_KEY = 'FETCH_TEMP_KEY'
-export const REQUEST_API_KEY = 'REQUEST_API_KEY'
-export const RECEIVE_API_KEY = 'RECEIVE_API_KEY'
-export const REQUEST_NEW_TEMP_KEY = 'REQUEST_NEW_TEMP_KEY'
-export const NEW_TEMP_KEY_SENT = 'NEW_TEMP_KEY_SENT'
-export const TEMP_KEY_IS_EXPIRED = 'TEMP_KEY_IS_EXPIRED'
+const REQUEST_API_KEY = 'REQUEST_API_KEY'
+const RECEIVE_API_KEY = 'RECEIVE_API_KEY'
+const REQUEST_NEW_TEMP_KEY = 'REQUEST_NEW_TEMP_KEY'
+const NEW_TEMP_KEY_SENT = 'NEW_TEMP_KEY_SENT'
+const TEMP_KEY_IS_EXPIRED = 'TEMP_KEY_IS_EXPIRED'
+const NETWORK_ERROR = 'NETWORK_ERROR'
+
+import { API_URL } from 'config/obelix.config'
 
 // ------------------------------------
 // Actions
@@ -16,20 +18,55 @@ export const TEMP_KEY_IS_EXPIRED = 'TEMP_KEY_IS_EXPIRED'
 
 // API Request
 export function fetchApiKey (tempKey) {
+  // Testing
   return dispatch => {
     dispatch(requestApiKey(tempKey))
-    return fetch(`https://httpbin.org/cache`)
+    const header = new Headers({
+      'tempaccesstoken': tempKey
+    })
+    const init = {
+      method: 'GET',
+      headers: header,
+      mode: 'cors',
+      cache: 'default'
+    }
+    const request = new Request(`${API_URL}auth/apiKey`, init)
+    return fetch(request)
         .then(response => {
-          return response.json()
+          if (response.status === 200) {
+            return response.json()
+          } else if (response.status === 401) {
+            return networkError()
+          }
         })
         .then(json => {
-          console.log(tempKey)
-          return dispatch(receiveApiKey(tempKey, json))
-          // return dispatch(tempKeyIsExpired())
+          if (json.apiKey) {
+            return dispatch(receiveApiKey(json.apiKey, json.partner_id))
+          } else {
+            return dispatch(tempKeyIsExpired())
+          }
         }
             )
-            .then(auth => dispatch(fetchUserProfile(auth.apiKey)))
-            .catch(err => console.log(err))
+            .then(auth => {
+              dispatch(fetchUserProfile(auth.apiKey, auth.partnerID))
+            })
+            .catch(err => {
+              console.log(err)
+              return dispatch(networkError())
+            }
+          )
+  }
+}
+
+function networkError () {
+  return {
+    type: NETWORK_ERROR
+  }
+}
+
+function tempKeyIsExpired () {
+  return {
+    type: TEMP_KEY_IS_EXPIRED
   }
 }
 
@@ -40,16 +77,11 @@ export function requestApiKey (tempKey = null) {
   }
 }
 
-export function receiveApiKey (tempKey, json) {
+export function receiveApiKey (apiKey, partnerID) {
   return {
     type: RECEIVE_API_KEY,
-    apiKey: json.origin
-  }
-}
-
-function tempKeyIsExpired () {
-  return {
-    type: TEMP_KEY_IS_EXPIRED
+    apiKey,
+    partnerID
   }
 }
 
@@ -64,11 +96,23 @@ export function fetchApiKeyifNeeded (tempKey) {
 export function sendNewTempKeyRequest (tempKey) {
   return dispatch => {
     dispatch(requestNewTempKeyPending(tempKey))
-    return fetch(`https://httpbin.org/cache`)
-    .then(response => {
-      return response.json
+    const header = new Headers({
+      'tempaccesstoken': tempKey
     })
-    .then(json => dispatch(requestNewTempKeySent(json)))
+    const init = {
+      method: 'GET',
+      headers: header,
+      mode: 'cors',
+      cache: 'default'
+    }
+    const request = new Request(`${API_URL}auth/tempToken/${tempKey}`, init)
+    return fetch(request)
+    .then(response => {
+      return response.json()
+    })
+    .then(json => {
+      dispatch(requestNewTempKeySent(json))
+    })
     .catch(err => console.log(err))
   }
 }
@@ -79,9 +123,11 @@ function requestNewTempKeyPending (tempKey) {
   }
 }
 
-function requestNewTempKeySent (tempKey) {
+function requestNewTempKeySent (json) {
   return {
-    type: NEW_TEMP_KEY_SENT
+    type: NEW_TEMP_KEY_SENT,
+    tempKeySentToEmail: json.email
+
   }
 }
 
@@ -93,13 +139,14 @@ const ACTION_HANDLERS_AUTH = {
     return Object.assign({}, state, { isFetching: true, tempKeyIsExpired: false, tempKey: action.tempKey, newTempKeyBy: '' })
   },
   [RECEIVE_API_KEY] : (state, action) => {
-    return Object.assign({}, state, { apiKey: action.apiKey, isFetching: false, tempKeyIsExpired: false })
+    return Object.assign({}, state, { apiKey: action.apiKey, partnerID: action.partner_id, isFetching: false, tempKeyIsExpired: false })
   },
   [REQUEST_NEW_TEMP_KEY]: (state, action) => Object.assign({}, state, { isFetching: true }),
-  [NEW_TEMP_KEY_SENT]: (state, action) => Object.assign({}, state, { isFetching: false, newTempKeyBy: state.tempKey, tempKey: '' }),
+  [NEW_TEMP_KEY_SENT]: (state, action) => Object.assign({}, state, { isFetching: false, newTempKeyBy: state.tempKey, tempKey: '', tempKeySentToEmail: action.tempKeySentToEmail }),
   [TEMP_KEY_IS_EXPIRED]: (state, action) => {
     return Object.assign({}, state, { tempKeyIsExpired: true, isFetching: false, apiKey: '' })
-  }
+  },
+  [NETWORK_ERROR]: (state, action) => Object.assign({}, state, { networkError: true })
 }
 
 
