b79aa962aaaf4ceb59006d9c48303a19417a52ed
Andreas <andreas.schadauer@gmail.com>
2017-02-07 14:27:49 +0100

    Extend Auth component

--- src/components/Auth/components/Auth.js
+++ src/components/Auth/components/Auth.js
@@ -1,35 +1,54 @@
 import React, { Component } from 'react'
+import Overlay from '../../Overlay/components/Overlay'
 import '../Auth.scss'
-
 
 class Auth extends Component {
 
   static propTypes = {
-    tempKeyIsExpired : React.PropTypes.bool,
+    auth: React.PropTypes.object.isRequired,
+    tempKeyIsExpired: React.PropTypes.bool,
     sendNewTempKeyRequest: React.PropTypes.func,
     newTempKeyBy: React.PropTypes.string
   }
 
   render () {
+    var message = null
+    if (this.props.auth.networkError) {
+      message = <Overlay
+        header='Leider konnten wir Ihre Daten nicht aufrufen.'
+        subheader='Es besteht derzeit ein Netzwerkproblem. Bitte versuchen Sie es später noch einmal.'
+        text='Sollte dieses Problem länger bestehen, bitten wir Sie, mit uns in Verbindung zu treten.'
+        icon='loading'
+        mode='stop' />
+    } else if (this.props.auth.tempKeyIsExpired) {
+      if (!this.props.auth.newTempKeyBy && !this.props.auth.isFetching && this.props.auth.tempKey !== null) {
+        message = <Overlay
+          header='Ihr Zugangslink ist nicht mehr gültig.'
+          subheader='Kein Problem, Sie können hier einfach einen neuen anfordern.'
+          text='Für die Sicherheit Ihrer Daten, können Sie Zugangslinks immer nur einmal verwenden.' 
+          button={() => this.props.sendNewTempKeyRequest(this.props.auth.tempKey)}
+          buttonText='Zugangslink per E-Mail anfordern'
+          icon='lock'
+       />
+      } else if (this.props.auth.tempKey === null) {
+        message = message = <Overlay
+          header='Sie haben keinen Berechtigung für diese Seite'
+          subheader='Sie können nur mit einem gültigen Zugangslink Ihre Daten abfragen.'
+          text='Für die Sicherheit Ihrer Daten, können Sie Zugangslinks immer nur einmal verwenden.'
+          icon='lock'
+       />
+      } else {
+        message = <Overlay
+        header={`Wir haben Ihnen einen neuen Link an ${this.props.auth.tempKeySentToEmail} gesendet.`}
+        subheader='Sollten Sie diese E-Mail nicht erhalten, oder wenn Sie generelle Probleme mit dieser Seite haben, kontaktieren Sie bitte unsere Spender- und Patenbetreuung.'
+        text=''
+        icon='loading'
+        mode='stop' />
+      }
+    }
+
     return (
-      <div>
-        {this.props.auth.tempKeyIsExpired &&
-        <div className='overlay'>
-          {!this.props.auth.newTempKeyBy && !this.props.auth.isFetching &&
-          <div className='overlay-content'>
-            <h1>Ihr Zugangslink ist leider nicht mehr gültig.</h1>
-            <h3>Kein Problem, Sie können hier einfach einen neuen anfordern.</h3>
-            <p>Für die Sicherheit Ihrer Daten, können Sie Zugangslinks immer nur einmal verwenden.</p>
-            <button onClick={this.props.sendNewTempKeyRequest}>Jetzt neuen Link per E-Mail anfordern</button>
-          </div>}
-          {this.props.auth.newTempKeyBy &&
-          <div className='overlay-content'>
-            <h1>Wir haben Ihnen einen neuen Link an Ihre E-Mailaddresse gesendet.</h1>
-            <p>Sollten Sie diese E-Mail nicht erhalten, oder wenn Sie generelle Probleme mit dieser Seite haben, kontaktieren Sie bitte unsere Spender- und Patenbetreuung.</p>
-          </div>}
-        </div>}
-     
-      </div>
+        message
     )
   }
 }
