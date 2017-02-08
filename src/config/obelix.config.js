ec25e936f745ca3b30b8e8b26db811a267d2d54e
Andreas <andreas.schadauer@gmail.com>
2017-02-08 17:28:21 +0100

    Add title object

--- src/config/obelix.config.js
+++ src/config/obelix.config.js
@@ -6,3 +6,190 @@ export const SALUTATION_CODE = {
 }
 
 export const API_URL = 'https://api.worldvision.at/api/'
+
+export const TITLES = [
+  {
+    code: '1',
+    description: 'cand. tech.'
+  },
+  {
+    code: '10',
+    description: 'DI (FH)'
+  },
+  {
+    code: '11',
+    description: 'Dipl.-Vw.'
+  },
+  {
+    code: '12',
+    description: 'Dkfm.'
+  },
+  {
+    code: '13',
+    description: 'Dkfm. DDr.'
+  },
+  {
+    code: '14',
+    description: 'Dkfm. Dr.'
+  },
+  {
+    code: '15',
+    description: 'Dkfm. Ing.'
+  },
+  {
+    code: '16',
+    description: 'Dkfm. Mag.'
+  },
+  {
+    code: '17',
+    description: 'Dr.'
+  },
+  {
+    code: '18',
+    description: 'Ing.'
+  },
+  {
+    code: '19',
+    description: 'Ing. Dr.'
+  },
+  {
+    code: '2',
+    description: 'cand. phil.'
+  },
+  {
+    code: '20',
+    description: 'Ing. Mag.'
+  },
+  {
+    code: '21',
+    description: 'Mag.'
+  },
+  {
+    code: '22',
+    description: 'Mag. DDr.'
+  },
+  {
+    code: '23',
+    description: 'Mag. Dr.'
+  },
+  {
+    code: '24',
+    description: 'MMag. Dr.'
+  },
+  {
+    code: '25',
+    description: 'Prof.'
+  },
+  {
+    code: '26',
+    description: 'Prof. Dr.'
+  },
+  {
+    code: '27',
+    description: 'Univ. Prof.'
+  },
+  {
+    code: '28',
+    description: 'Univ. Prof. Dr.'
+  },
+  {
+    code: '29',
+    description: 'Dir.'
+  },
+  {
+    code: '3',
+    description: 'DDDr.'
+  },
+  {
+    code: '30',
+    description: 'Komm.Rat'
+  },
+  {
+    code: '31',
+    description: 'Pfarrer'
+  },
+  {
+    code: '32',
+    description: 'Lektor'
+  },
+  {
+    code: '33',
+    description: 'MMag.'
+  },
+  {
+    code: '34',
+    description: 'OSR'
+  },
+  {
+    code: '35',
+    description: 'Oberrat Dr.'
+  },
+  {
+    code: '36',
+    description: 'Ao. Univ.-Prof. Dr.'
+  },
+  {
+    code: '37',
+    description: 'Hofrat'
+  },
+  {
+    code: '38',
+    description: 'Ministerialrat'
+  },
+  {
+    code: '39',
+    description: 'Dipl.-Bw'
+  },
+  {
+    code: '4',
+    description: 'DDI'
+  },
+  {
+    code: '40',
+    description: 'Mag. (FH)'
+  },
+  {
+    code: '41',
+    description: 'Dipl.Päd.'
+  },
+  {
+    code: '42',
+    description: 'DGKS'
+  },
+  {
+    code: '43',
+    description: 'BA'
+  },
+  {
+    code: '44',
+    description: 'MA'
+  },
+  {
+    code: '45',
+    description: 'MTA'
+  },
+  {
+    code: '46',
+    description: 'Prälat'
+  },
+  {
+    code: '5',
+    description: 'DDr.'
+  },
+  {
+    code: '6',
+    description: 'DI'
+  },
+  {
+    code: '7',
+    description: 'DI DDr.'
+  },
+  {
+    code: '8',
+    description: 'DI Dr.'
+  },
+  {
+    code: '9',
+    description: 'DI Mag.'
+  }
+]
