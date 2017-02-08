bcf74a25f0ebbec46b594f49b0e5f3ab473d1c73
Andreas <andreas.schadauer@gmail.com>
2017-02-08 17:27:33 +0100

    Css tweaks

--- src/components/Overlay/components/Overlay.js
+++ src/components/Overlay/components/Overlay.js
@@ -5,29 +5,30 @@ const Overlay = (props) => {
   return (
     <div className='overlay'>
       <div className='container'>
-          <div className='content center'>
-            {props.icon === 'loading' &&
-              <div className='loading'>
-                <div className={props.mode}></div>
-              </div>
+        <div className='content center'>
+          {props.icon === 'loading' &&
+            <div className='loading'>
+              <div className={props.mode} />
+            </div>
             }
+          <div className='icon'>
             {props.icon === 'lock' &&
-              <div className='icon-lock'>
-                <div className='lock-top-1' ></div>
-                <div className='lock-top-2'></div>
-                <div className='lock-body'></div>
-                <div className='lock-hole'></div>
-              </div>
-            }
-              
-              <div>
-                <h1>{props.header}</h1>
-                <h3>{props.subheader}</h3>
-                {props.button &&
-                <button onClick={props.button}>{props.buttonText}</button>
-                }
-              </div>
+            <div className='icon-lock'>
+              <div className='lock-top-1' />
+              <div className='lock-top-2' />
+              <div className='lock-body' />
+              <div className='lock-hole' />
+            </div>
+              }
           </div>
+          <div className='message'>
+            <h1>{props.header}</h1>
+            <h3>{props.subheader}</h3>
+            {props.button &&
+            <button onClick={props.button}>{props.buttonText}</button>
+            }
+          </div>
+        </div>
       </div>
     </div>
   )
